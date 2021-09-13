a370d2664800938dc29b1361f8449bcc57950848
hussein <husseinfakih019@gmail.com>
2021-09-12 21:37:30 -0400

    project transfer

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js';
+
+function App() {
+  return (
+    <>
+    <Particles
+    params={{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area:900
+          }
+        }
+
+      }
+    }}
+    
+    />
+    <Navbar/>
+    <Header/>
+    </>
+  
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = ()=> {
+
+    return(
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Web developement and websites promotions</h1>
+                <Typed
+                className="typed-text"
+                strings={["Web Design", "Web Development","Facebook Ads SMM","Google Ads"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+
+                />
+
+                <a href="#" className="btn-main-offer"> Contact Me</a>
+            </div>
+     </div>
+
+    )
+}
+
+export default Header;
